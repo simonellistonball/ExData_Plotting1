97a3ad6ae08df5d9a2839a741b0e73def5b6c59b
Simon Elliston Ball <simon@simonellistonball.com>
2014-10-10 03:57:06 +0100

    Added script to export the PNGs required

---
+++ make_pngs.R
@@ -0,0 +1,12 @@
+#source('load_and_clean.R')
+
+make_png <- function(name) {
+  png(filename = paste(name, 'png', sep = '.'), width = 480, height = 480)
+  source(paste(name, 'R', sep = '.'))
+  dev.off()
+}
+
+make_png('plot1')
+make_png('plot2')
+make_png('plot3')
+make_png('plot4')
